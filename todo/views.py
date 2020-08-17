4ff93a89ab491869b4e086e9ec0b4f459a8be380
Jitgitbit <rowowolf@gmail.com>
2020-08-17 13:32:36 +0200

    Error Handling

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
 def signupuser(request):
@@ -9,8 +11,16 @@ def signupuser(request):
 	else:
 		# Create a new user
 		if request.POST['password1'] == request.POST['password2']:
-			User.objects.create_user(request.POST['username'], request.POST['password1'])
-			user.save()
+			try:
+				user = User.objects.create_user(request.POST['username'], request.POST['password1'])
+				user.save()
+				login(request, user)
+				return redirect('currenttodos')
+			except IntegrityError:
+				return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'That username has been taken already. Please choose a different username'})
 		else:
 			# Tell the user the passwords didn't match
-			print("Passwords don't match!")
+			return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+
+def currenttodos(request):
+	return render(request, 'todo/currenttodos.html')
